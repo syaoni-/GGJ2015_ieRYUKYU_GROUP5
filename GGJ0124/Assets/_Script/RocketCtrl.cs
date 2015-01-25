3db946c247811820328845c8d5d41d31bea59716
syaony <e125752@ie.u-ryukyu.ac.jp>
2015-01-25 09:15:55 +0900

    ライブラリの消去

---
+++ GGJ0124/Assets/_Script/RocketCtrl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketCtrl : MonoBehaviour {
+
+	public Vector2 aTargetPos;
+	public float aGunShotTime;
+
+	private Texture explode;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Update();
+	}
+}
---
+++ GGJ0124/Assets/_Script/RocketExplodeCtrl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketExplodeCtrl : MonoBehaviour {
+
+	private float aTime;
+	private float aDeleteTime = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+		aTime += Time.deltaTime;
+		if (aTime > aDeleteTime)
+			Object.Destroy(this.gameObject);
+	}
+}
